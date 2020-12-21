909f036a7e60b18906ff67f6e07c64c6150e0048
dzwdz <kg67199@gmail.com>
2020-12-21 18:54:03 +0100

    fancy shell colors

--- shell/.config/fish/config.fish
+++ shell/.config/fish/config.fish
@@ -1,1 +1,8 @@
 alias py python
+
+set fish_color_command red
+set fish_color_param brblack
+set fish_color_operator green
+set fish_color_autosuggestion cyan
+set fish_color_error white
+set fish_color_search_match --background='000'
