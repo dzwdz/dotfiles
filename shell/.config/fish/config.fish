267e151f0277e6009af3adef0c2617267b73f502
dzwdz <kg67199@gmail.com>
2020-12-24 12:13:03 +0100

    fish cdpath

--- shell/.config/fish/config.fish
+++ shell/.config/fish/config.fish
@@ -6,3 +6,5 @@ set fish_color_operator green
 set fish_color_autosuggestion cyan
 set fish_color_error white
 set fish_color_search_match --background='000'
+
+set CDPATH . /hdd/repo/current ~/Documents ~
