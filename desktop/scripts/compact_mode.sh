5445d77277e02a9a1dbacc440dde6a9cd7f4a2c0
dzwdz <kg67199@gmail.com>
2021-01-05 16:15:46 +0100

    compact mode thing

---
+++ desktop/scripts/compact_mode.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+if [ -e /tmp/compact_mode ]
+then
+	polybar-msg cmd show
+	i3-msg gaps inner all set 4
+	rm /tmp/compact_mode
+else
+	polybar-msg cmd hide
+	i3-msg gaps inner all set 0
+	touch /tmp/compact_mode
+fi
