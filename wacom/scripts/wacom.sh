9e40fb62cb8f9711659107b213b05f0678beabe6
dzwdz <kg67199@gmail.com>
2021-01-10 22:26:19 +0100

    wacom stuff

--- wacom/scripts/wacom.sh
+++ wacom/scripts/wacom.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-xsetwacom set 11 Button 2 12
-xsetwacom set 11 Button 3 13
+xsetwacom set "Wacom Intuos PT S Pen stylus" Button 2 12
+xsetwacom set "Wacom Intuos PT S Pen stylus" Button 3 13
+xsetwacom set "Wacom Intuos PT S Pen stylus" MapToOutput HEAD-1
 xbindkeys
-touch ~/testtest
