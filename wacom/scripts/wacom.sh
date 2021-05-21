639779c5b73d55a4ca874ace91565dffc3aa629a
dzwdz <kg67199@gmail.com>
2021-05-21 09:55:03 +0200

    some wacom stuff

--- wacom/scripts/wacom.sh
+++ wacom/scripts/wacom.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 xsetwacom set "Wacom Intuos PT S Pen stylus" Button 2 12
 xsetwacom set "Wacom Intuos PT S Pen stylus" Button 3 13
-xsetwacom set "Wacom Intuos PT S Pen stylus" MapToOutput HEAD-0
-xsetwacom set "Wacom Intuos PT S Pen eraser" MapToOutput HEAD-0
+xsetwacom set "Wacom Intuos PT S Pen stylus" MapToOutput HEAD-1
+xsetwacom set "Wacom Intuos PT S Pen eraser" MapToOutput HEAD-1
 xbindkeys
