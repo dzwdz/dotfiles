d8fc54c76fb2746c61e1b5ece45d794a110fcc73
dzwdz <kg67199@gmail.com>
2021-06-03 21:10:30 +0200

    better emacs config

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -3,22 +3,15 @@
              '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
-(add-to-list 'load-path "~/.emacs.d/ext/org-krita")
-(require 'org-krita)
 
+;; EVIL
 (unless (package-installed-p 'evil)
   (package-install 'evil))
-(unless (package-installed-p 'general)
+(unless (package-installed-p 'general) ; used for the jk binding
   (package-install 'general))
-(unless (package-installed-p 'selectrum)
-  (package-install 'selectrum))
-(unless (package-installed-p 'gruvbox-theme)
-  (package-install 'gruvbox-theme))
-(unless (package-installed-p 'org-roam)
-  (package-install 'org-roam))
 
-(require 'general)
 (require 'evil)
+(require 'general)
 (evil-mode 1)
 (general-evil-setup)
 (general-imap "j"
@@ -26,39 +19,81 @@
     :timeout 0.1
     "k" 'evil-normal-state))
 
+(with-eval-after-load 'evil-maps ; don't fuck up org mode
+  (define-key evil-motion-state-map (kbd "SPC") nil)
+  (define-key evil-motion-state-map (kbd "RET") nil)
+  (define-key evil-motion-state-map (kbd "TAB") nil))
+
+					;
+;; incremental search
+(unless (package-installed-p 'selectrum)
+  (package-install 'selectrum))
 (require 'selectrum)
 (selectrum-mode +1)
+(setq completion-ignore-case t)
 
-(setq inhibit-startup-message t)
-(menu-bar-mode -1)
-(toggle-scroll-bar -1)
-(tool-bar-mode -1)
+;; org mode setup
+(add-to-list 'load-path "~/.emacs.d/ext/org-krita")
+(require 'org-krita)
+(unless (package-installed-p 'org-roam)
+  (package-install 'org-roam))
+
+(add-hook 'after-init-hook 'org-roam-mode)
+(setq org-roam-directory "~/Notes")
+(setq org-agenda-files '("~/Notes"))
+
+(setq org-startup-with-inline-images t)
+(setq org-startup-with-latex-preview t)
+; makes the LaTeX preview bigger
+(setq org-format-latex-options '(:foreground default      :background default       :scale 2.0
+			    :html-foreground "Black" :html-background "Transparent" :html-scale 1.0
+			    :matchers ("begin" "$1" "$" "$$" "\\(" "\\[")))
+; disable splitting
+(setq org-link-frame-setup
+   '((vm . vm-visit-folder-other-frame)
+     (vm-imap . vm-visit-imap-folder-other-frame)
+     (gnus . org-gnus-no-new-news)
+     (file . find-file)
+     (wl . wl-other-frame)))
+
+(global-set-key (kbd "C-c n") 'org-roam-find-file)
+(global-set-key (kbd "C-c i") 'org-roam-insert)
+(setq org-return-follows-link t)
+
+
+;; making emacs pretty
+(unless (package-installed-p 'gruvbox-theme)
+  (package-install 'gruvbox-theme))
 (load-theme 'gruvbox-light-soft t)
 
-(global-set-key (kbd "C-s") 'save-buffer)
 
-; modeline
-(set-face-attribute 'default nil :font "Jetbrains Mono" :height 120)
+(set-face-attribute 'default nil :font "JetBrains Mono" :height 120)
 (let ((bg "#d5c4a1"))
   (set-face-attribute 'mode-line nil
 		      :box `(:line-width 8 :color ,bg)
 		      :background bg))
-
 (blink-cursor-mode 0)
 (setq-default cursor-type 'bar)
 
-(setq org-startup-with-inline-images t)
+(menu-bar-mode -1)
+(toggle-scroll-bar -1)
+(tool-bar-mode -1)
 
+(setq org-hide-leading-stars nil)
+(set-face-attribute 'org-document-title nil :font "Liberation Serif" :height 3.0 :foreground "#32302f")
+
+
+;; show the index on load
+(setq inhibit-startup-message t)
 (find-file "~/Notes/index.org")
-(setq org-roam-directory "~/Notes")
-(add-hook 'after-init-hook 'org-roam-mode)
-(global-set-key (kbd "C-c n") 'org-roam-find-file)
-(global-set-key (kbd "C-c i") 'org-roam-insert)
-(setq org-return-follows-link t)
-(setq org-hide-leading-stars t)
 
+
+;; misc
+(global-set-key (kbd "C-s") 'save-buffer)
 (setq backup-directory-alist '(("." . "~/tmp/emacs")))
 
-(setq completion-ignore-case t)
 
-(setq org-agenda-files '("~/Notes"))
+
+
+
+
