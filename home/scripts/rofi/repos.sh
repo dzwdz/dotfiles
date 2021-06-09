1a1e586201204bb68d30e1c60eaecbee6535c0fe
dzwdz <kg67199@gmail.com>
2021-06-09 11:52:24 +0200

    spring cleaning

--- home/scripts/rofi/repos.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [ -z "$1"]; then
-	ls /hdd/repo/current
-else
-	urxvt -cd "/hdd/repo/current/$1" &
-fi
--- sync_w.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# syncs a small subset of dotfiles with a remote server
-scp ~/.vimrc $1:~/.vimrc
