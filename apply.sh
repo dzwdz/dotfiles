25856cc71c9468078ecdd2fb7810977d18b2d7ba
dzwdz <kg67199@gmail.com>
2021-09-24 21:08:17 +0200

    stowless apply.sh

--- apply.sh
+++ apply.sh
@@ -1,2 +1,15 @@
 #!/usr/bin/env sh
-stow -t ~ home
+
+# TODO ensure that this is run in the right dir
+PREFIX=~
+
+mkdir -p $PREFIX
+ln -sr home/.bin $PREFIX/.bin
+ln -sr home/.vim $PREFIX/.vim
+
+for orig in $(git ls-files | grep home/)
+do
+	target=$PREFIX$(echo $orig | sed s/home//)
+	mkdir -p $(dirname $target)
+	ln -sr $orig $target
+done
